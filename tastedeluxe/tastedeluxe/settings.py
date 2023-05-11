e248ff6c468c40d53f0441ec3ec5ba7cb4b7501d
0Neocoda0 <neocoda.work@gmail.com>
2023-05-11 05:18:02 +0300

    static root

--- tastedeluxe/tastedeluxe/settings.py
+++ tastedeluxe/tastedeluxe/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "content/static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -144,5 +145,6 @@ LOGIN_URL = 'login'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-    
+
+
 
