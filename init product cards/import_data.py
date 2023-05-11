2c0635ccf640d83b2de87cc1219a7b20ef68670c
0Neocoda0 <neocoda.work@gmail.com>
2023-05-11 06:13:46 +0300

    update import data db

--- init product cards/import_data.py
+++ init product cards/import_data.py
@@ -2,23 +2,7 @@ import csv
 import psycopg2
 import os
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="tastedeluxe",
-    user="aishat",
-    password="=0Ze=!g)pi>WU~0"
-)
-cur = conn.cursor()
-file_list = []
-dirlist = os.listdir()
-
-for filename in dirlist:
-    if '.csv' in filename:
-        file_list.append(filename)
-
 def write_data(file_name):
-    # Открытие курсора для выполнения запросов к базе данных
-    
     # Чтение данных из CSV файла и вставка их в базу данных
     with open(file_name, 'r') as f:
         reader = csv.reader(f, delimiter=',')
@@ -27,12 +11,28 @@ def write_data(file_name):
             cur.execute(
                 "INSERT INTO products_productcard (name, price, image, category, food_type) VALUES (%s, %s, %s, %s, %s);",
                 (row[0], row[1], row[2], row[3], row[4]))
-    # Сохранение изменений в базе данных и закрытие соединения
-    
 
+
+file_list = []
+dirlist = os.listdir()
+
+for filename in dirlist:
+    if '.csv' in filename:
+        file_list.append(filename)
+
+
+# Открытие курсора для выполнения запросов к базе данных
+conn = psycopg2.connect(
+    host="localhost",
+    database="tastedeluxe",
+    user="aishat",
+    password="=0Ze=!g)pi>WU~0"
+)
+cur = conn.cursor()
 
 for name in file_list:
     write_data(name)
+   # Сохранение изменений в базе данных и закрытие соединения
 
 conn.commit()
 cur.close()
