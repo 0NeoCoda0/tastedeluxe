4be6b51be0619c6b066cc870050f58237865f6c4
0Neocoda0 <neocoda.work@gmail.com>
2023-05-11 06:08:01 +0300

    impoert data update

--- init product cards/import_data.py
+++ init product cards/import_data.py
@@ -1,7 +1,7 @@
 import csv
 import psycopg2
+import os
 
-# Соединение с базой данных PostgreSQL
 conn = psycopg2.connect(
     host="localhost",
     database="tastedeluxe",
@@ -9,20 +9,30 @@ conn = psycopg2.connect(
     password="=0Ze=!g)pi>WU~0"
 )
 
-# Открытие курсора для выполнения запросов к базе данных
-cur = conn.cursor()
+file_list = []
+dirlist = os.listdir()
 
-# Чтение данных из CSV файла и вставка их в базу данных
-with open('hot-food.csv', 'r') as f:
-    reader = csv.reader(f, delimiter=',')
-    next(reader)  # пропускаем заголовок файла
-    for row in reader:
-        cur.execute(
-            "INSERT INTO products_productcard (name, price, image, category, food_type) VALUES (%s, %s, %s, %s, %s);",
-            (row[0], row[1], row[2], row[3], row[4])
-        )
+for filename in dirlist:
+    if '.csv' in filename:
+        file_list.append(filename)
 
-# Сохранение изменений в базе данных и закрытие соединения
-conn.commit()
-cur.close()
-conn.close()
+def write_data(file_name):
+    # Открытие курсора для выполнения запросов к базе данных
+    cur = conn.cursor()
+    # Чтение данных из CSV файла и вставка их в базу данных
+    with open(file_name, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        next(reader)  # пропускаем заголовок файла
+        for row in reader:
+            cur.execute(
+                "INSERT INTO products_productcard (name, price, image, category, food_type) VALUES (%s, %s, %s, %s, %s);",
+                (row[0], row[1], row[2], row[3], row[4]))
+    # Сохранение изменений в базе данных и закрытие соединения
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+for name in file_list:
+    write_data(name)
+
